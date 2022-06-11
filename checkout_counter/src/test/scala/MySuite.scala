6ad593242dea3784d5bccf762a66a72b15f2b1ee
Lacramioara Astefanoaei <lacramioara.astefanoaei@nomadic-labs.com>
2022-06-11 09:58:40 +0200

    add basic test

--- checkout_counter/src/test/scala/MySuite.scala
+++ checkout_counter/src/test/scala/MySuite.scala
@@ -1,9 +1,11 @@
+import checkout.Checkout_Products
+
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends munit.FunSuite {
   test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
+    val obtained = Checkout_Products.checkout(Map(("book1", 2), ("book3", 3), ("book4", 1)), Map(("book1", 10), ("book3", 14)))._2
+    val expected = 62.0
     assertEquals(obtained, expected)
   }
 }
