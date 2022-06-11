4dd744bb272279964b9fb979c4fcee1cbac52999
Lacramioara Astefanoaei <lacramioara.astefanoaei@nomadic-labs.com>
2022-06-11 09:52:46 +0200

    load products from csv

--- checkout_counter/src/main/scala/Main.scala
+++ checkout_counter/src/main/scala/Main.scala
@@ -1,7 +1,21 @@
 package checkout
 
-def products = Map(("book1", 10.0), ("book2",Double.NaN), ("book3", 15.0), ("book4", 16.0))
+import scala.io.Source
+
+def load_products() : Map[String, Double] = {
+  var products = collection.mutable.Map[String, Double]()
+  /* adapted from https://www.oreilly.com/library/view/scala-cookbook/9781449340292/ch12s06.html */
+  val bufferedSource = Source.fromFile("products.csv")
+  for (line <- bufferedSource.getLines) {
+    val cols = line.split(",").map(_.trim)
+    val price = try { cols{1}.toDouble } catch { case _ => Double.NaN }
+    products += (cols{0} -> price)
+  }
+  bufferedSource.close
+  return products.toMap
+}
 
 @main def hello: Unit =
-  println(Checkout_Products.checkout(Map(("book1", 2), ("book4", 3)), products))
+  println(load_products())
+  println(Checkout_Products.checkout(Map(("book1", 2), ("book3", 3), ("book4", 1)), load_products()))
 
